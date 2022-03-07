4acb25eb92041c77a87dec952665b3ec430215e7
ismailastighfar <73545680+ismailastighfar@users.noreply.github.com>
2022-03-07 11:02:43 +0100

    add changes

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,8 +15,13 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('username');
+            $table->integer('role');
+            $table->string('phoneNumber')->unique()->nullable();
+            $table->string('gender');
+            $table->date('dateOfBirth');
             $table->string('email')->unique();
+            $table->string('adress')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
