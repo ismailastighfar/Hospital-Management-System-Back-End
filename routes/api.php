08f6b84397da84aa69a123bf9e68c976053e0551
ismailastighfar <73545680+ismailastighfar@users.noreply.github.com>
2022-03-10 11:15:58 +0100

    add changes

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,9 @@
 
 use App\Http\Controllers\DepartmentController;
 use App\Http\Controllers\DoctorController;
+use App\Http\Controllers\MedicineController;
 use App\Http\Controllers\UserController;
+
 
 use App\Models\Department;
 use GuzzleHttp\Middleware;
@@ -20,16 +22,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/doctors' , [DoctorController::class, 'index']);
-Route::get('/doctors/{user}' , [DoctorController::class, 'show']);
-Route::post('/doctors' , [DoctorController::class, 'store']);
-Route::put('/doctors/{doctor}' , [DoctorController::class, 'update']);
+Route::resource('doctors' , DoctorController::class);
 
+Route::resource('medicines' , MedicineController::class);
 
+Route::get('/medicines/search/{name}', [MedicineController::class, 'search']);
 
 Route::resource('users' , UserController::class);
 
 Route::resource('departments' , DepartmentController::class);
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
